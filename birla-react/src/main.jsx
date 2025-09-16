9696691b3a3fb11173b60418a7e59ca70f3c8c9a
Yash-Kudve <yash.kudve@deptagency.com>
2025-09-16 17:24:51 +0530

    unwanted code removal and components added

--- birla-react/src/main.jsx
+++ birla-react/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Segment from './components/Segment/Segment.jsx'
+import Inspiration from './components/Inspiration/Inspiration.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Segment />
+    <Inspiration />
   </StrictMode>,
 )
